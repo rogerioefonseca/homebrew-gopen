abe6c82ad9f07dd472ff6f2bd02b08fa4b18c103
Rogério Fonseca <rfonseca@bigpoint.net>
2022-04-07 09:13:05 +0200

    Updating homebrew props

--- gopen.rb
+++ gopen.rb
@@ -1,6 +1,9 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gopen < Formula
   desc "Go binary that opens the actual git repo url in a browser"
-  homepage "https://rogerioefonseca.github.io/homebrew-gopen/"
+  homepage "https://github.com/rogerioefonseca/gopen"
   url "https://github.com/rogerioefonseca/gopen/releases/download/v0.0.9/gopen-darwin-amd64.tar.gz"
   sha256 "ff4e9e2e4943cb55fb87e24763e2bd770a92be7eaee2e389a302d322413c7be4"
   license "MIT"
@@ -9,9 +12,5 @@ class Gopen < Formula
 
   def install
     bin.install "gopen"
-  end
-
-  test do
-    system "#{bin}/gopen"
   end
 end
